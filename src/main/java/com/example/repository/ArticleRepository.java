12c1d149332a2adb8efc3b536a7a07584b8eee4d
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-24 19:25:50 +0900

    ArticleRepository.javaを作成

---
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -0,0 +1,27 @@
+package com.example.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Article;
+
+@Repository
+public class ArticleRepository {
+
+	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
+
+		Article article = new Article();
+
+		article.setId(rs.getInt("id"));
+		article.setName(rs.getString("name"));
+		article.setContent(rs.getString("content"));
+
+		return article;
+	};
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+}
