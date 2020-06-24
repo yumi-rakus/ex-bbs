1b278eb16631b8b52a8587d25bfdc559e528d83b
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-24 20:50:11 +0900

    ArticleController.javaにフォームをrequestスコープに格納するsetUpArticleForm()、記事を投稿するpost()を追加

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Article;
+import com.example.form.ArticleForm;
 import com.example.repository.ArticleRepository;
 
 @Controller
@@ -19,6 +21,19 @@ public class ArticleController {
 	@Autowired
 	private ArticleRepository articleRepository;
 
+	/**
+	 * 使用するフォームオブジェクトをリクエストスコープに格納する.
+	 * 
+	 * @return フォーム
+	 */
+	@ModelAttribute
+	public ArticleForm setUpArticleForm() {
+		return new ArticleForm();
+	}
+
+	/////////////////////////////////////////////////////
+	// ユースケース：記事一覧を表示する
+	/////////////////////////////////////////////////////
 	/**
 	 * 掲示板画面を表示する.
 	 * 
@@ -32,7 +47,33 @@ public class ArticleController {
 
 		model.addAttribute("articleList", articleList);
 
-		return "bbc";
+		return "bbs";
+	}
+
+	/////////////////////////////////////////////////////
+	// ユースケース：記事を投稿する
+	/////////////////////////////////////////////////////
+	/**
+	 * 記事を投稿する.
+	 * 
+	 * @param form  フォーム
+	 * @param model モデル
+	 * @return 掲示板画面
+	 */
+	@RequestMapping("/post")
+	public String post(ArticleForm form, Model model) {
+
+		Article article = new Article();
+
+		article.setName(form.getName());
+		article.setContent(form.getContent());
+
+		articleRepository.insert(article);
+
+		form.setName("");
+		form.setContent("");
+
+		return index(model);
 	}
 
 }
