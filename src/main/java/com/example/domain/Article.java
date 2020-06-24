fc27b377e841cda945a810122aab3ada3c173609
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-24 19:24:47 +0900

    Article.javaを作成

---
+++ src/main/java/com/example/domain/Article.java
@@ -0,0 +1,84 @@
+package com.example.domain;
+
+import java.util.List;
+
+/**
+ * 記事情報を表すドメイン.
+ * 
+ * @author yumi takahashi
+ *
+ */
+public class Article {
+
+	/**
+	 * ID
+	 */
+	private Integer id;
+
+	/**
+	 * 投稿者名
+	 */
+	private String name;
+
+	/**
+	 * 投稿内容
+	 */
+	private String content;
+
+	/**
+	 * コメントリスト
+	 */
+	private List<Comment> commentList;
+
+	// constructor
+	public Article() {
+	}
+
+	public Article(Integer id, String name, String content, List<Comment> commentList) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.content = content;
+		this.commentList = commentList;
+	}
+
+	// getter setter
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<Comment> getCommentList() {
+		return commentList;
+	}
+
+	public void setCommentList(List<Comment> commentList) {
+		this.commentList = commentList;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", content=" + content + ", commentList=" + commentList + "]";
+	}
+
+}
