a0e6bd44040d341f5a3f6a45174f9cad252518c4
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-24 19:39:11 +0900

    ArticleRepository.javaに全記事情報一覧を取得するfindAll()を追加

--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -1,5 +1,7 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -23,5 +25,19 @@ public class ArticleRepository {
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
+
+	/**
+	 * 記事情報を全件取得する.
+	 * 
+	 * @return 記事情報一覧
+	 */
+	public List<Article> findAll() {
+
+		String sql = "SELECT * FROM articles ORDER BY id DESC";
+
+		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
+
+		return articleList;
+	}
 
 }
