0c1705bd68b12e9eb901d1631019a98462b43e56
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-24 20:52:37 +0900

    ArticleRepository.javaに記事情報を挿入するinsert()を追加

--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Article;
@@ -38,6 +40,22 @@ public class ArticleRepository {
 		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
 
 		return articleList;
+	}
+
+	/**
+	 * 記事情報を挿入する.
+	 * 
+	 * @param article 記事情報
+	 */
+	public void insert(Article article) {
+
+		String sql = "INSERT INTO articles (name, content) VALUES (:name, :content)";
+
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", article.getName()).addValue("content",
+				article.getContent());
+
+		template.update(sql, param);
+
 	}
 
 }
