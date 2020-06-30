2ff187c2a008f080fbf2c5fcbd5e13cba9ad471c
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-06-30 11:07:35 +0900

    CommentForm.javaのフィールド変数にFormのindex番号を表すcommentFormListindexと記事情報を表すarticleを追加

--- src/main/java/com/example/form/CommentForm.java
+++ src/main/java/com/example/form/CommentForm.java
@@ -3,6 +3,8 @@ package com.example.form;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.example.domain.Article;
+
 /**
  * コメント投稿情報を受け取るフォーム.
  * 
@@ -14,7 +16,12 @@ public class CommentForm {
 	/**
 	 * 記事ID
 	 */
-	private String articleId;
+	private Integer articleId;
+
+	/**
+	 * commentFormのindex番号
+	 */
+	private Integer commentFormListindex;
 
 	/**
 	 * 名前
@@ -30,13 +37,26 @@ public class CommentForm {
 	@Size(max = 140, message = "コメント内容は140字以内で入力してください")
 	private String content;
 
+	/**
+	 * 記事情報
+	 */
+	private Article article;
+
 	// getter setter
-	public String getArticleId() {
+	public Integer getArticleId() {
 		return articleId;
 	}
 
-	public void setArticleId(String articleId) {
+	public void setArticleId(Integer articleId) {
 		this.articleId = articleId;
+	}
+
+	public Integer getCommentFormListindex() {
+		return commentFormListindex;
+	}
+
+	public void setCommentFormListindex(Integer commentFormListindex) {
+		this.commentFormListindex = commentFormListindex;
 	}
 
 	public String getName() {
@@ -55,10 +75,19 @@ public class CommentForm {
 		this.content = content;
 	}
 
+	public Article getArticle() {
+		return article;
+	}
+
+	public void setArticle(Article article) {
+		this.article = article;
+	}
+
 	// toString
 	@Override
 	public String toString() {
-		return "CommentForm [articleId=" + articleId + ", name=" + name + ", content=" + content + "]";
+		return "CommentForm [articleId=" + articleId + ", commentFormListindex=" + commentFormListindex + ", name="
+				+ name + ", content=" + content + ", article=" + article + "]";
 	}
 
 }
